3bd6bf9bb50450f77845b62cbb8097c96174885e
beboche <david.baux@inserm.fr>
2017-10-10 16:47:02 +0200

    initial commit
     Initial commit

---
+++ import_batch_led.sh
@@ -0,0 +1,124 @@
+#/bin/bash
+
+
+VERSION=1.0
+USAGE="
+Program: import_batch_led
+Version: ${VERSION}
+Contact: Baux David <david.baux<at>inserm.fr>
+
+Usage: $(basename "$0") [options] -- program to import a batch of VCF and txt files into LED. Single VCF output for all samples.
+
+Example:
+sh import_batch_led.sh -d path/to/vcf/folder/ [-p '.final' ]
+
+Arguments:
+
+    -d,     --directory path to the directory containing the vcf and txt files
+    -p,     --prefix    prefix to use to name vcf files see example below
+
+Example text file:
+
+#patient_id	less than 10 chars
+#family_id	less than 10 chars
+#gender		m/f
+#disease_name	RP,DFNB,DFNA,USH,ATAXIA,MYOPATHY,HEALTHY,CF,CF-RD,CBAVD
+#team_name	SENSORINEURAL,NEUROMUSCULAR,ATAXIA,MUCO
+#visibility	0/1
+#experiment	trusight_one,exome_ss_v6,cftr_complete
+patient_id:1558
+family_id:318
+gender:f
+disease_name:CF
+team_name:MUCO
+visibility:1
+experiment_type:cftr_complete
+
+name of txt file and of vcf file must match,
+e.g.
+1558.txt and 1558.vcf
+unless -p is used
+
+if you have 1558.txt and 1558.final.vcf use -p '.final'
+
+"
+IMPORT_SCRIPT=import_vcf.pl
+DOS2UNIX=/usr/local/bin/dos2unix
+
+
+##############		If no options are given, print help message	#################################
+
+if [ $# -eq 0 ]; then
+	echo "${USAGE}"
+	echo "Error Message : No arguments provided"
+	echo ""
+	exit 1
+fi
+
+
+###############		Get arguments from command line			#################################
+
+while [[ "$#" -gt 0 ]]
+do
+KEY="$1"
+case "${KEY}" in
+    -d|--directory)					#mandatory
+    DIR="$2"
+    shift
+    ;;
+    -p|--prefix)
+    PREFIX="$2"
+    shift
+    ;;
+    -h|--help)
+    echo "${USAGE}"
+    exit 1
+    ;;
+    *)
+    echo "Error Message : Unknown option ${KEY}" 	# unknown option
+    exit
+    ;;
+esac
+shift
+done
+
+########	add / to INPUT_PATH if needed
+if [[ "${DIR}" =~ .+[^\/]$ ]];then
+	DIR="${DIR}/"
+fi
+
+#get file list from $DIR
+FILES=$(ls -l ${DIR} | grep -v '^d' | awk '{print $9}' | grep -e '.txt$')
+
+for FILE in ${FILES}
+do
+    if [[ "${FILE}" =~ (.+)\.txt ]];then
+        #for each text file we build the expected vcf name
+        VCF="${BASH_REMATCH[1]}${PREFIX}.vcf"
+        if [ -f "${DIR}/${VCF}" ];then
+            #if name is ok
+            #dos2unix conversion then launch import script
+            ${DOS2UNIX} ${DIR}${FILE}
+            if [ "$?" -eq 0 ];then
+                ${DOS2UNIX} ${DIR}${VCF}
+                if [ "$?" -eq 0 ];then
+                    ./"${IMPORT_SCRIPT}" -p "${DIR}${FILE}" -i "${DIR}${VCF}" -c
+                    if [ "$?" -eq 0 ];then
+                        echo "${BASH_REMATCH[1]} done"
+                    else
+                        echo "pb with ${FILE}"
+                    fi
+                else
+                    echo "Permissions pb with file ${DIR}${VCF}"
+                fi
+            else
+                echo "Permissions pb with file ${DIR}${FILE}"
+            fi
+        else
+            echo "No vcf file matching ${FILE}"
+        fi
+        
+    fi
+done
+
+exit
---
+++ import_vcf.pl
@@ -0,0 +1,143 @@
+#! /usr/bin/perl -wT
+
+use strict;
+use DBI;
+use Getopt::Std;
+
+$Getopt::Std::STANDARD_HELP_VERSION = 1;
+
+##########################################################################################################
+##	Script to import vcf files into lgm_ex								##
+##	david baux 04/2016										##
+##	david.baux@inserm.fr										##
+##########################################################################################################
+
+
+my (%opts, $pat_file, $vcf);
+getopts('i:p:c', \%opts);
+
+if ((not exists $opts{'i'}) || ($opts{'i'} !~ /\.vcf/o) || (not exists $opts{'p'}) || ($opts{'p'} !~ /\.txt/o)) {
+	&HELP_MESSAGE();
+	exit
+}
+if ($opts{'i'} =~ /(.+\.vcf)$/o) {$vcf = $1} 
+if ($opts{'p'} =~ /(.+\.txt)$/o) {$pat_file = $1}
+if ($opts{'c'}) {
+	open(F, $vcf) or die $!;
+	my ($i, $warning) = (0, 'f');
+	while (<F>) {
+		chomp;
+		$i++;
+		#print "$i\n";
+		if (/^#/o) {next}
+		elsif (/^[^c]/o) {print "\nCheck line $i in $vcf\nAborting $pat_file\n";$warning = 't';}	
+	}
+	if ($warning eq 't') {exit}
+}
+
+
+my $dbh = DBI->connect(    "DBI:Pg:database=lgm_ex;host=localhost;",
+                        'lgm',
+                        'genetique1',
+                        {'RaiseError' => 1}
+                ) or die $DBI::errstr;
+
+
+#########	get patient info from file			#########
+my $patient;
+open(F, $pat_file) or die $!;
+
+while (<F>) {
+	if (/^#/o) {next}
+	chomp;
+	my @info = split(/:/, $_);
+	$patient->{pop @info} = pop @info;
+}
+
+close F;
+
+#foreach my $key (keys(%{$patient})) {
+#	print "$key:$patient->{$key}\n"
+#}
+
+#my $output = $patient->{'patient_id'}."_".$patient->{'experiment_type'}.".sql";
+
+
+#########	get date					#########
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+my $month = ($mon+1);
+if ($month < 10) {$month = "0$month"}
+if ($mday < 10) {$mday = "0$mday"}
+my $date =  (1900+$year)."-$month-".$mday;
+
+
+my $sql = "INSERT INTO PATIENT (patient_id,family_id,gender,disease_name,team_name,visibility,creation,experiment_type) VALUES ('$patient->{'patient_id'}','$patient->{'family_id'}','$patient->{'gender'}','$patient->{'disease_name'}','$patient->{'team_name'}','$patient->{'visibility'}','$date','$patient->{'experiment_type'}');";
+#print $sql;exit;
+$dbh->do($sql);
+my $query_patient = "SELECT id FROM Patient WHERE patient_id = '$patient->{'patient_id'}' AND family_id = '$patient->{'family_id'}' AND gender = '$patient->{'gender'}' AND disease_name = '$patient->{'disease_name'}' AND experiment_type = '$patient->{'experiment_type'}';";
+my $res_patient = $dbh->selectrow_hashref($query_patient);
+#print "$sql\n";
+
+#########	get variant info from vcf			#########
+my $genome;
+my ($i, $j, $h) = (0, 0, 0);
+open(F, $vcf) or die $!;
+
+while (<F>) {
+	chomp;
+	$i++;
+	if (/^#/o) {$h++}
+	if (/^##reference=.+(hg\d{2})/o) {$genome = $1}
+	elsif ((/^#CHROM/o) && (!$genome)) {print "\nFATAL: No reference genome found\n";exit 1;}
+	elsif (/^chr/o || /^[\dXYM]{1,2}\s+/o) {		
+		my @line = split(/\t/, $_);
+		if ($line[9] !~ /^0\/0:/o) {
+			my ($chr,$pos,$rs,$ref,$alt,$qual,$filter) = (shift(@line),shift(@line),shift(@line),shift(@line),shift(@line),shift(@line),shift(@line));
+			#some cleaning
+			if ($chr =~ /chr([\dXYM]{1,2})/o) {$chr = $1}		
+			if ($rs eq '.') {$rs = 'NULL'}
+			elsif ($rs =~ /rs(\d+)/o) {$rs = "'$1'"}
+			#variant already known?
+			my $query = "SELECT id FROM Variant WHERE chr = '$chr' AND pos_$genome = '$pos' AND reference = '$ref' AND alternative = '$alt';";
+			my $res = $dbh->selectrow_hashref($query);
+			if (!$res) {
+				#variant does not exists, should be created
+				$sql = "INSERT INTO Variant (chr,pos_$genome,reference,alternative,dbsnp_rs,creation) VALUES ('$chr','$pos','$ref','$alt',$rs,'$date');";
+				$dbh->do($sql);
+				#print "$sql\n";
+			}
+			$query = "SELECT id FROM Variant WHERE chr = '$chr' AND pos_$genome = '$pos' AND reference = '$ref' AND alternative = '$alt';";
+			$res = $dbh->selectrow_hashref($query);
+			#we need status and doc
+			my $status = 'heterozygous';
+			if (($chr eq 'Y') || ($chr eq 'X' && $patient->{'gender'} eq 'm')) {$status = 'hemizygous'}
+			#print $line[0];exit;
+			if ($line[0] =~ /AF=1\.00;/o) {$status = 'homozygous'}
+			my $doc;
+			if ($line[0] =~ /DP=(\d+);/o) {$doc = $1}
+			elsif ($line[0] =~ /TC=(\d+);/o) {$doc = $1}
+			$sql = "INSERT INTO Variant2patient (variant_id,patient_id,status_type,filter,qual,doc) VALUES ('$res->{'id'}','$res_patient->{'id'}','$status','$filter','$qual','$doc')";
+			$dbh->do($sql);
+			$j++;
+			#print "$sql\n";
+			#TODO:check if annotations are complete in Variant table
+		}
+		#else {print $line[9]}
+	}
+	
+}
+print "\n".($i-$h)." lines in VCF $vcf and $j variants recorded\n";
+
+sub HELP_MESSAGE {
+	print "\nUsage: ./import_vcf.pl -i path/to/vcf/file.vcf -p path/to/patient/file.txt \nSupports --help or --version\n\n
+### This script imports vcf files into lgm_ex
+### -i vcf file from MSR, NENUFAAR ANNOVAR or NENUFAAR alone
+### -p patient file
+### -c check mode: checks vcf for unwanted \n (all lines must begin with # or c) prior to insertion. Abort if finds sthg
+### contact: david.baux\@inserm.fr\n\n"
+}
+
+sub VERSION_MESSAGE {
+	print "\nVersion 0.1 18/04/2016\n"
+}
